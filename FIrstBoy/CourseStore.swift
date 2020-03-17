adf7057422ae572711aac0a54cd76f91f6cb4a97
Mazen.A <40984264+MZ195@users.noreply.github.com>
2020-03-17 16:49:24 +0300

    36th lesson, not done yet

---
+++ FIrstBoy/CourseStore.swift
@@ -0,0 +1,16 @@
+//
+//  CourseStore.swift
+//  FIrstBoy
+//
+//  Created by Mazen .A on 17/03/2020.
+//  Copyright © 2020 Mazen .A. All rights reserved.
+//
+
+import SwiftUI
+import Contentful
+
+let client = Client(spaceId: "0ge8xzmnbp2c", environmentId: "master", accessToken: "03010b0d79abcb655ca3fda453f2f493b5472e0aaa53664bc7dea5ef4fce2676")
+
+func getArray() {
+    
+}
